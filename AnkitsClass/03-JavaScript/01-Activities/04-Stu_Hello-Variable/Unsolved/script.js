594585026ffee5fade30e76fc912b8051a4f43dd
gunnysensei <gwagoner@instructors.2u.com>
2023-02-13 19:57:25 -0600

    adding solutions JS day 1

--- AnkitsClass/03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ AnkitsClass/03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,9 @@
 // Write Your JavaScript Code Here
+
+var personName = "Gunner";
+var pets = 7;
+var funFact = "Shoot your shot";
+
+console.log("My name is", personName);
+console.log("I have", pets, "pets.");
+console.log("My fun fact is " + funFact);
